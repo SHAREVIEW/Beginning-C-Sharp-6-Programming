e6073ff67302e3a07f3dd83f19f5329f9e0bef70
harrypoirot <harrypoirot@live.com>
2017-04-28 01:43:47 +0800

    Ex01 done.

---
+++ Chapter13/Ex01/Ex01/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Ex01
+{
+    //public delegate void MessageHandler(string messageText);
+    public class Connection
+    {
+        public event EventHandler<MessageArriveEventArgs> MessageArrived;
+        public string Name { get; set; }
+        private Timer pollTimer;
+        public Connection()
+        {
+            pollTimer = new Timer(100);
+            pollTimer.Elapsed += new ElapsedEventHandler(CheckForMessage);
+        }
+        public void Connect() => pollTimer.Start();
+        public void Disconnection() => pollTimer.Stop();
+        private static Random random = new Random();
+        private void CheckForMessage(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Checking for new messages.");
+            if ((random.Next(9) == 0) && (MessageArrived != null))
+            {
+                MessageArrived(this,new MessageArriveEventArgs("Hello Mami"));
+            }
+        }
+    }
+}
